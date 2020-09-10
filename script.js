5ef8b055c4147973b9b16a77d79b17d61527f8f8
Ike Ah-Loe <ike@pop-os.localdomain>
2020-09-10 05:41:34 -0500

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Write your JavaScript code here!
+// Write your JavaScript code here!  
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
